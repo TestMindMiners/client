4f9456aede837255ebf51af8f3a273ae1d86918e
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-02-28 21:25:53 -0300

    feat(Table): create Table component
    created Table component

---
+++ src/components/table/Table.js
@@ -0,0 +1,91 @@
+import "./Table.css";
+
+export default function Table() {
+  return (
+    <article className="table_background">
+      <table className="table">
+        <thead className="table_title">
+          <tr>
+            <th>{"Data da Operação"}</th>
+            <th>{"Tipo (Compra/Venda)"}</th>
+            <th>{"Preço"}</th>
+            <th>{"Quantidade"}</th>
+            <th>{"Taxa de Corretagem"}</th>
+          </tr>
+        </thead>
+        <tbody className="table_data">
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+          <tr>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+            <td>value</td>
+          </tr>
+        </tbody>
+      </table>
+    </article>
+  );
+}
