7450dddf4ed7bc125e5a506a34cb3dcd5ae6c2b0
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-02 14:30:18 -0300

    feat(Select): add name property

--- src/components/select/Select.js
+++ src/components/select/Select.js
@@ -6,7 +6,7 @@ export default function Select(props) {
       <label className="element_title" htmlFor={props.selectName}>
         {props.name}
       </label>
-      <select className="input select" id={props.selectName}>
+      <select className="input select" id={props.selectName} name={props.selectName}>
         {props.options
           ? props.options.map((item, index) => (
               <option value={item.value} key={index}>
