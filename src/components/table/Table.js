bd0538d04a75870219ba648b71c660337868d724
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-02 14:30:49 -0300

    refact(Table): move Api request to home

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,32 +1,11 @@
-import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Api } from "../../api/Api";
 import "./Table.css";
 
-export default function Table() {
-  const [operationList, setOperationList] = useState([]);
-  const getOperations = async () => {
-    let result = [];
-    const response = await Api.GetRequest(Api.selectOperationUrl());
-    const operations = await response.json();
-    operations.forEach((item) => {
-      const dateOperation = new Date(item.operationDate).toLocaleDateString();
-      result.push({
-        id: item.id,
-        operationDate: dateOperation,
-        operationType: item.operationType,
-        price: item.operationPrice,
-        quantity: item.operationQuantity,
-        brockerageFee: item.brockerageFee,
-      });
-    });
-    setOperationList(result);
-  };
-  useEffect(() => {
-    getOperations();
-  }, []);
+export default function Table(props) {
   return (
+    
     <article className="table_background">
+
       <table className="table">
         <thead className="table_title">
           <tr>
@@ -39,14 +18,16 @@ export default function Table() {
             <th>opções</th>
           </tr>
         </thead>
+        {props.operations?
         <tbody className="table_data">
-          {operationList.map((item) => (
+          
+          {props.operations.map((item) => (
             <tr key={item.id}>
               <td>{item.id}</td>
-              <td>{item.operationDate}</td>
+              <td>{new Date(item.operationDate).toLocaleDateString()}</td>
               <td>{item.operationType}</td>
-              <td>{item.price}</td>
-              <td>{item.quantity}</td>
+              <td>{item.operationPrice}</td>
+              <td>{item.operationQuantity}</td>
               <td>{item.brockerageFee}</td>
               <td>
                 <Link to={`/view/${item.id}`}>view</Link>
@@ -54,6 +35,7 @@ export default function Table() {
             </tr>
           ))}
         </tbody>
+        :""}
       </table>
     </article>
   );
