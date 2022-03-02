0f042512e4e32a5354bd7436c005cdd4af82ff24
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-02 03:41:39 -0300

    feat(View):create page View

---
+++ src/pages/view/View.js
@@ -0,0 +1,36 @@
+import { useNavigate } from 'react-router-dom';
+import {useState, useEffect} from "react";
+import { useParams } from "react-router-dom";
+import { Api } from "../../api/Api";
+import OperationViewer from '../../components/operationViewer/OperationViewer';
+import Button from '../../components/button/Button';
+import './View.css';
+
+
+export default function View(){
+    const params = useParams();
+    const navigate = useNavigate();
+    const [operationData,setOperationData] = useState();
+    const getOperationDataValues = async()=>{
+        const response = await Api.GetRequest(Api.selectOperationByIdUrl(params.id));
+        const operation = await response.json();
+        setOperationData(operation);
+    }
+    const goBack = (event)=>{
+        event.preventDefault();
+        navigate(-1);
+    }
+    useEffect(()=>{
+        getOperationDataValues();
+    },[])
+    return (
+        <section className='page'>
+        {operationData?
+            <OperationViewer operationData={operationData}></OperationViewer>
+            :
+            ""
+        }
+        <Button buttonText={"Voltar"} buttonName={"normal_button"} click={goBack} />
+        </section>
+    );
+}
