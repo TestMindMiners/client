f6954df45165d0d5038c740b8b9ddf11933fc274
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-02 03:45:21 -0300

    refact(PageRoutes): add new route

--- src/routes/PageRoutes.js
+++ src/routes/PageRoutes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
+import View from "../pages/view/View";
 export default function PageRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/view/:id" element={<View />} />
       </Routes>
     </BrowserRouter>
   );
