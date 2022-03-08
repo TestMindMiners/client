fa5cda2a12980ee28f4cf6a70c7aca6ae9e903b6
HIKARO CARVALHO <81428096+hikarocarvalho@users.noreply.github.com>
2022-03-08 00:49:31 -0300

    refact(Table): move Api request to component

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,41 +1,61 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Api } from "../../api/Api";
 import "./Table.css";
 
 export default function Table(props) {
+  const [dataOperations, setDataOperations] = useState();
+
+  const getOperations = async () => {
+    const response = await Api.GetRequest(Api.selectOperationUrl());
+    const operationsList = await response.json();
+    setDataOperations(operationsList)
+  };
+
+  useEffect(()=>{
+    getOperations();
+  },[]);
+
   return (
-    <article className="table_background">
-      <table className="table">
-        <thead className="table_title">
-          <tr>
-            <th>id</th>
-            <th>{"Data da Operação"}</th>
-            <th>{"Tipo (Compra/Venda)"}</th>
-            <th>{"Preço"}</th>
-            <th>{"Quantidade"}</th>
-            <th>{"Taxa de Corretagem"}</th>
-            <th>opções</th>
-          </tr>
-        </thead>
-        {props.operations ? (
-          <tbody className="table_data">
-            {props.operations.map((item) => (
-              <tr key={item.id}>
-                <td>{item.id}</td>
-                <td>{new Date(item.operationDate).toLocaleDateString()}</td>
-                <td>{item.operationType}</td>
-                <td>{item.operationPrice}</td>
-                <td>{item.operationQuantity}</td>
-                <td>{item.brockerageFee}</td>
-                <td>
-                  <Link to={`/view/${item.id}`}>view</Link>
-                </td>
+    <>
+      {dataOperations ? (
+        <article className="table_background">
+          <table className="table">
+            <thead className="table_title">
+              <tr>
+                <th>id</th>
+                <th>{"Data da Operação"}</th>
+                <th>{"Tipo (Compra/Venda)"}</th>
+                <th>{"Preço"}</th>
+                <th>{"Quantidade"}</th>
+                <th>{"Taxa de Corretagem"}</th>
+                <th>opções</th>
               </tr>
-            ))}
-          </tbody>
-        ) : (
-          ""
-        )}
-      </table>
-    </article>
+            </thead>
+            {dataOperations ? (
+              <tbody className="table_data">
+                {dataOperations.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{new Date(item.operationDate).toLocaleDateString()}</td>
+                    <td>{item.operationType}</td>
+                    <td>{item.operationPrice}</td>
+                    <td>{item.operationQuantity}</td>
+                    <td>{item.brockerageFee}</td>
+                    <td>
+                      <Link to={`/view/${item.id}`}>view</Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            ) : (
+              null
+            )}
+          </table>
+        </article>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
